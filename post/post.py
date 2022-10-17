7d58cb4b7911ae8c049fed9316e7eecde9522360
Егор Портнягин <erportnyagin@edu.hse.ru>
2022-10-18 02:43:35 +0300

    another

---
+++ post/post.py
@@ -0,0 +1,31 @@
+import datetime
+import json
+import requests
+
+url = 'http://192.168.240.144:7100'
+headers = {"charset": "utf-8", 'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+with open("transactions.json", 'r', encoding='utf-8') as f:
+    data = dict(json.load(f)['transactions'])
+
+count = 0
+time_now = datetime.datetime.now()
+sum_time = datetime.datetime.now()
+for item in data:
+    count += 1
+    d = {'id': int(item)}
+    for k, v in data[item].items():
+        d[k] = v
+    d['amount'] = float(d['amount'])
+    d['passport'] = str(d['passport'])
+
+    response = requests.post(url, data=json.dumps(d), headers=headers)
+    if count % 100 == 0:
+        print(count, datetime.datetime.now()-time_now)
+        time_now = datetime.datetime.now()
+
+    # print(count, item, response, d)
+    # if count == 10:
+    #     break
+print(f'end total:{count}')
+print(datetime.datetime.now()-sum_time)
