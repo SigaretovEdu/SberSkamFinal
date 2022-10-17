ef2018e2f4e095643c6c1e54298cc46067d80560
Егор Портнягин <erportnyagin@edu.hse.ru>
2022-10-17 10:54:12 +0300

    all my friends are toxic

--- flask1/hello.py
+++ flask1/hello.py
@@ -7,25 +7,24 @@ from os.path import exists
 from geopy.distance import geodesic
 import datetime
 
+frod_types = ['to_old_or_young',
+              'night_time',
+              'same_card_num',
+              'fast_operations',
+              'many_declines',
+              'decreasing_operation_sum',
+              'invalid_password',
+              'interrupt_in_card_values',
+              'noname']
 
-frod_types=['to_old_or_young',
-'night_time',
-'same_card_num',
-'fast_operations',
-'many_declines',
-'decreasing_operation_sum',
-'invalid_password',
-'interrupt_in_card_values',
-'noname']
-
-rating ="""CREATE TABLE IF NOT EXISTS client_rating (
+rating = """CREATE TABLE IF NOT EXISTS client_rating (
     client_id CHARACTER VARYING (10),
     rating REAL,
     day_operations INT,
     night_operations INT
 );"""
 
-validat="""CREATE TABLE IF NOT EXISTS validation (
+validat = """CREATE TABLE IF NOT EXISTS validation (
     passport CHARACTER VARYING (25),
     passport_valid_to CHARACTER VARYING (25)
 )"""
@@ -61,13 +60,11 @@ select_last_n = """
     SELECT * FROM transactions WHERE client=%s ORDER BY transaction_id DESC LIMIT %s  
 """
 
-
 pon = (
     'transaction_id', 'date', 'card', 'account', 'account_valid_to', 'client', 'last_name', 'first_name',
     'patronymic',
     'date_of_birth', 'passport', 'passport_valid_to', 'phone', 'oper_type', 'amount', 'oper_result', 'terminal',
     'terminal_type', 'city', 'address')
-
 
 add_command = """
     INSERT INTO transactions (date,card,account,account_valid_to,client,last_name,first_name,patronymic,date_of_birth,passport,passport_valid_to,phone,oper_type,amount,oper_result,terminal,terminal_type,city,address) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
@@ -87,12 +84,10 @@ def init(data: dict):
 @app.route('/', methods=['POST', 'GET'])
 def mail():
     request_data = request.json
+
     bill = 0
     if check_age(request_data):
         bill += 0.5
-
-
-
 
     with connection.cursor() as cursor:
         cursor.execute(add_command, init(request_data))
@@ -172,142 +167,51 @@ def check_multiple_passport_validation(data: dict, client_rating: dict, f=0):
     return answer
 
 
-def check_failed_account_validation(data: dict, client_rating: dict, f=0):
-    print('*****\ndetected for failed account validation')
-    answer = []
-    for item in data:
-        if datetime.datetime.strptime(data[item]['date'].split('T')[0], "%Y-%m-%d") > datetime.datetime.strptime(
-                data[item]['account_valid_to'].split('T')[0], "%Y-%m-%d"):
-            answer.append((item))
-            client_rating[data[item]['client']] -= 2
-    if f == 1:
-        for item in answer:
-            print(data[item])
-    print('total count: ', len(answer))
-    return answer
+def check_failed_account_validation(data: dict):
+    t1 = datetime.datetime.strptime(data['date'].split('T')[0], "%Y-%m-%d")
+    t2 = datetime.datetime.strptime(data['account_valid_to'].split('T')[0], "%Y-%m-%d")
+    if t1 > t2:
+        return True
+    else:
+        return False
 
 
-def check_brute(data: dict, client_rating: dict, f=0):
-    print('*****\ndetected for brute')
-    d = {}
-    for item in data:
-        if data[item]['client'] not in d.keys():
-            d[data[item]['client']] = [(item, data[item]['account'])]
-        else:
-            d[data[item]['client']].append((item, data[item]['account']))
-    an = set()
-    for k, v in d.items():
-        for i in range(0, len(v) - 1):
-            if check_diff(v[i][1], v[i + 1][1]):
-                an.add(v[i][0])
-                an.add(v[i + 1][0])
-    answer = sorted(an)
-    for item in answer:
-        client_rating[data[item]['client']] -= 5
-        if f == 1:
-            print(data[item])
-    print('total count: ', len(answer))
-    return answer
+def check_brute(l: list):
+    if check_diff(l[0]['account'], l[1]['account']) and check_diff(l[1]['account'], l[2]['account']):
+        return True
+    else:
+        return False
 
 
-def check_night_time(data: dict, client_rating: dict, limit, f=0):
-    print('*****\ndetected for most operations at night')
-    d = {}
-    for item in data:
-        if data[item]['client'] not in d.keys():
-            d[data[item]['client']] = [(item, data[item]['date'].split('T')[1])]
-        else:
-            d[data[item]['client']].append((item, data[item]['date'].split('T')[1]))
-    answer = []
-    for k, v in d.items():
-        if len(v) > 1:
-            count = 0
-            opers = []
-            for item in v:
-                if int(item[1].split(':')[0]) == 23 or int(item[1].split(':')[0]) < 6:
-                    count += 1
-                    opers.append(item[0])
-            if float(count) / len(v) > limit:
-                for item in opers:
-                    client_rating[data[item]['client']] -= 3
-                answer += opers
-    if f == 1:
-        for item in answer:
-            print(data[item])
-    print('total count: ', len(answer))
-    return answer
+def check_night_time(data: dict):
+    t = datetime.datetime.strptime(data['date'].split('T')[1], "%H:%M:%S")
+    t = t.hour
+    if t == 23 or t < 6:
+        return True
+    else:
+        return False
 
 
-def check_fast_operations(data: dict, client_rating: dict, time_interval: float, oper_number: int, f=0):
-    print('*****\ndetected in carrying out multiple operations in a short period of time')
-    answer = []
-    oper=[]
-    persons = []
-    day = []
-    hour = []
-    for item in data:
-        day.clear()
-        hour.clear()
-        counter = 1
-        temp = str(data[item]['client'])
-        oper.append(item)
-        if temp not in persons:
-            persons.append(temp)
-            for i in data:
-                if str(data[i]['client']) == temp:
-                    day.append(data[i]['date'].split('T')[0].split('-')[2])
-                    hour.append(data[i]['date'].split('T')[1])
-                    oper.append(i)
-            for i in range(len(day) - 1):
-                if int(day[i]) < int(day[i + 1]) - 1:
-                    i -= counter
-                    counter = 1
-                elif int(day[i]) * 1440 + int(hour[i].split(':')[0]) * 60 + int(hour[i].split(':')[1]) + int(
-                        hour[i].split(':')[1]) / 60 + time_interval > int(day[i + 1]) * 1440 + int(
-                    hour[i + 1].split(':')[0]) * 60 + int(hour[i + 1].split(':')[1]) + int(
-                    hour[i + 1].split(':')[1]) / 60:
-                    counter += 1
-                else:
-                    i -= counter - 1
-                    counter = 1
-                if counter == oper_number:
-                    client_rating[temp]-=7
-                    for j in range(i,i+counter):
-                        answer.append(oper[j])
-                    i+=counter
-                    counter=1
-    if f == 1:
-        for item in answer:
-            print(data[item])
-    print('total count: ', len(answer))
-    return answer
+def check_fast_operations(l: list, delta: int):
+    pause = 0
+    for i in range(0, len(l) - 1):
+        t1 = datetime.datetime.strptime(l[i]['date'], "%Y-%m-%dT%H:%M:%S")
+        t2 = datetime.datetime.strptime(l[i + 1]['date'], "%Y-%m-%dT%H:%M:%S")
+        pause += t2 - t1
+    ex = (len(l) - 1) * delta
+    ex = datetime.timedelta(minutes=ex)
+    if pause < ex:
+        return True
+    else:
+        return False
 
 
-def check_reduction_of_the_amount(data: dict, client_rating: dict, f=0):
-    print('*****\ndetected more than two consecutive withdrawal operations from the account')
-    answer = []
-    persons = {}
-    for item in data:
-        if data[item]['client'] not in persons:
-            persons[data[item]['client']] = []
-        persons[data[item]['client']].append(item)
-        persons[data[item]['client']].append(data[item]['oper_type'])
-        persons[data[item]['client']].append(data[item]['oper_result'])
-        persons[data[item]['client']].append(data[item]['amount'])
-    for per in persons:
-        for i in range(len(persons[per]) - 4):
-            if (persons[per][i+1] == 'Снятие' or persons[per][i+1] == 'Оплата') and (
-                    persons[per][i + 5] == 'Снятие' or persons[per][i + 5] == 'Оплата'):
-                if persons[per][i + 2] == 'Отказ' and persons[per][i + 6] == 'Отказ' and persons[per][i + 3] >= \
-                        persons[per][i + 7]:
-                    client_rating[per]-=7
-                    answer.append(persons[per][i])
-                    answer.append(persons[per][i+4])
-    if f == 1:
-        for item in answer:
-            print(data[item])
-    print('total count: ', len(answer))
-    return answer
+def check_reduction_of_the_amount(l: list):
+    if l[0]['oper_result'] == 'Отказ' and l[1]['oper_result'] == 'Отказ':
+        if l[0]['amount'] * 2 < l[0]['amount']:
+            return True
+    else:
+        return False
 
 
 def check_age(data: dict):
@@ -342,28 +246,11 @@ def check_personals(data, client_rating: dict, f=0):
     return answer
 
 
-def check_decline(data: dict, client_rating: dict, oper_number: int, f=0):
-    print('*****\ndetected in carrying out many rejected operations')
-    answer = []
-    persons = {}
-    for item in data:
-        if data[item]['client'] not in persons:
-            persons[data[item]['client']] = {'declined': 0, 'successfully': 0,'oper':[]}
-        if data[item]['oper_result'] == 'Успешно':
-            persons[data[item]['client']]['successfully'] += 1
-        else:
-            persons[data[item]['client']]['declined'] += 1
-            persons[data[item]['client']]['oper'].append(item)
-    for k in persons:
-        if persons[k]['declined'] >= oper_number:
-            client_rating[k]-=7
-            for i in persons[k]['oper']:
-                answer.append(i)
-    if f == 1:
-        for item in answer:
-            print(data[item])
-    print('total count: ', len(answer))
-    return answer
+def check_decline(l: list):
+    if l[0]['oper_result'] == 'Отказ' and l[1]['oper_result'] == 'Отказ' and l[2]['oper_result'] == 'Отказ':
+        return True
+    else:
+        return False
 
 
 def check_adress_distance(data, client_rating: dict, f=0):
@@ -410,6 +297,7 @@ def check_adress_distance(data, client_rating: dict, f=0):
             print(data[item])
     print('total count: ', len(answer))
     return answer
+
 
 def readall(l_n: str, f_n: str, pat: str):
     answer = {}
